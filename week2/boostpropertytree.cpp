2027626ef45adbb482a8a4d4f670cfce9cf3d950
Strunin Aleksey <47733959+striker10041@users.noreply.github.com>
2024-02-12 15:02:18 -0500

    Add files via upload

---
+++ week2/boostpropertytree.cpp
@@ -0,0 +1,70 @@
+﻿#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <sstream>
+#include <cstdlib>
+
+int main() {
+    auto start_read = std::chrono::high_resolution_clock::now();
+    std::ifstream file("generated.json");
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    auto end_read = std::chrono::high_resolution_clock::now();
+
+    auto start_parse = std::chrono::high_resolution_clock::now();
+    boost::property_tree::ptree pt;
+    boost::property_tree::read_json(buffer, pt);
+    auto end_parse = std::chrono::high_resolution_clock::now();
+
+    auto start_modify = std::chrono::high_resolution_clock::now();
+    for (auto& item : pt) {
+        item.second.put("_id", item.second.get<std::string>("_id") + " modified");
+        item.second.put("guid", item.second.get<std::string>("guid") + " modified");
+        item.second.put("balance", item.second.get<std::string>("balance") + " modified");
+        item.second.put("picture", item.second.get<std::string>("picture") + " modified");
+        item.second.put("eyeColor", item.second.get<std::string>("eyeColor") + " modified");
+        item.second.put("name", item.second.get<std::string>("name") + " modified");
+        item.second.put("gender", item.second.get<std::string>("gender") + " modified");
+        item.second.put("company", item.second.get<std::string>("company") + " modified");
+        item.second.put("email", item.second.get<std::string>("email") + " modified");
+        item.second.put("phone", item.second.get<std::string>("phone") + " modified");
+        item.second.put("address", item.second.get<std::string>("address") + " modified");
+        item.second.put("about", item.second.get<std::string>("about") + " modified");
+        item.second.put("registered", item.second.get<std::string>("registered") + " modified");
+        item.second.put("latitude", item.second.get<double>("latitude") + 1.0);
+        item.second.put("longitude", item.second.get<double>("longitude") + 1.0);
+        item.second.put("isActive", !item.second.get<bool>("isActive"));
+        item.second.put("age", item.second.get<int>("age") + 1);
+
+        for (auto& tag : item.second.get_child("tags")) {
+            tag.second.data() = tag.second.get_value<std::string>() + " modified";
+        }
+
+        for (auto& friendObj : item.second.get_child("friends")) {
+            friendObj.second.put("name", friendObj.second.get<std::string>("name") + " modified");
+        }
+
+        item.second.put("greeting", item.second.get<std::string>("greeting") + " modified");
+        item.second.put("favoriteFruit", item.second.get<std::string>("favoriteFruit") + " modified");
+    }
+    auto end_modify = std::chrono::high_resolution_clock::now();
+
+    auto start_write = std::chrono::high_resolution_clock::now();
+    std::ofstream outputFileStream("output_boost.json");
+    boost::property_tree::write_json(outputFileStream, pt);
+    auto end_write = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double, std::milli> read_duration = end_read - start_read;
+    std::chrono::duration<double, std::milli> parse_duration = end_parse - start_parse;
+    std::chrono::duration<double, std::milli> modify_duration = end_modify - start_modify;
+    std::chrono::duration<double, std::milli> write_duration = end_write - start_write;
+
+    std::cout << "Boost Property Tree Read time: " << read_duration.count() << " ms\n";
+    std::cout << "Boost Property Tree Parse time: " << parse_duration.count() << " ms\n";
+    std::cout << "Boost Property Tree Modify time: " << modify_duration.count() << " ms\n";
+    std::cout << "Boost Property Tree Write time: " << write_duration.count() << " ms\n";
+
+    system("pause");
+}
---
+++ week2/jsoncpp.cpp
@@ -0,0 +1,77 @@
+﻿#include <json/json.h>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <sstream>
+#include <cstdlib>
+
+int main() {
+    auto start_read = std::chrono::high_resolution_clock::now();
+    std::ifstream file("generated.json");
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    auto end_read = std::chrono::high_resolution_clock::now();
+
+    auto start_parse = std::chrono::high_resolution_clock::now();
+    Json::Value root;
+    Json::Reader reader;
+    bool parsingSuccessful = reader.parse(buffer, root);
+    auto end_parse = std::chrono::high_resolution_clock::now();
+
+    if (!parsingSuccessful) {
+        std::cerr << "Error while parsing JSON." << std::endl;
+        return 1;
+    }
+
+    auto start_modify = std::chrono::high_resolution_clock::now();
+    for (Json::Value& item : root) {
+        item["_id"] = item["_id"].asString() + " modified";
+        item["guid"] = item["guid"].asString() + " modified";
+        item["isActive"] = !item["isActive"].asBool();
+        item["balance"] = item["balance"].asString() + " modified";
+        item["picture"] = item["picture"].asString() + " modified";
+        item["age"] = item["age"].asInt() + 1;
+        item["eyeColor"] = item["eyeColor"].asString() + " modified";
+        item["name"] = item["name"].asString() + " modified";
+        item["gender"] = item["gender"].asString() + " modified";
+        item["company"] = item["company"].asString() + " modified";
+        item["email"] = item["email"].asString() + " modified";
+        item["phone"] = item["phone"].asString() + " modified";
+        item["address"] = item["address"].asString() + " modified";
+        item["about"] = item["about"].asString() + " modified";
+        item["registered"] = item["registered"].asString() + " modified";
+        item["latitude"] = item["latitude"].asDouble() + 1.0;
+        item["longitude"] = item["longitude"].asDouble() + 1.0;
+
+        for (Json::Value& tag : item["tags"]) {
+            tag = tag.asString() + " modified";
+        }
+
+        for (Json::Value& friendObj : item["friends"]) {
+            friendObj["name"] = friendObj["name"].asString() + " modified";
+        }
+
+        item["greeting"] = item["greeting"].asString() + " modified";
+        item["favoriteFruit"] = item["favoriteFruit"].asString() + " modified";
+    }
+    auto end_modify = std::chrono::high_resolution_clock::now();
+
+    auto start_write = std::chrono::high_resolution_clock::now();
+    std::ofstream outputFileStream("output_jsoncpp.json");
+    Json::StreamWriterBuilder builder;
+    std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
+    writer->write(root, &outputFileStream);
+    auto end_write = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double, std::milli> read_duration = end_read - start_read;
+    std::chrono::duration<double, std::milli> parse_duration = end_parse - start_parse;
+    std::chrono::duration<double, std::milli> modify_duration = end_modify - start_modify;
+    std::chrono::duration<double, std::milli> write_duration = end_write - start_write;
+
+    std::cout << "JsonCpp Read time: " << read_duration.count() << " ms\n";
+    std::cout << "JsonCpp Parse time: " << parse_duration.count() << " ms\n";
+    std::cout << "JsonCpp Modify time: " << modify_duration.count() << " ms\n";
+    std::cout << "JsonCpp Write time: " << write_duration.count() << " ms\n";
+
+    system("pause");
+}
